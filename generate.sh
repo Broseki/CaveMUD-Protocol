41014051945f805e4ede05b9f4acccd236c4dbf4
Broseki <broseki@cavefox.net>
2023-09-30 21:01:45 -0400

    Added generator for protocol files

---
+++ generate.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Check if an argument was provided
+if [ -z "$1" ]
+    then
+        echo "No argument supplied. Please provide the type of source files to generate (ex: cpp, python, etc)"
+        exit 1
+fi
+
+# Create the generated directory if it doesn't exist
+mkdir -p generated
+
+# Generate the source files for each .proto file in the src directory
+for file in src/*.proto
+do
+    filename=$(basename -- "$file")
+    extension="${filename##*.}"
+    filename="${filename%.*}"
+
+    # Generate the source files based on the provided argument
+    case "$1" in
+        cpp)
+            protoc --cpp_out=generated "$file"
+            ;;
+        python)
+            protoc --python_out=generated "$file"
+            ;;
+        *)
+            echo "Invalid argument. Please provide a valid type of source files to generate (ex: cpp, python, etc)"
+            exit 1
+            ;;
+    esac
+done
