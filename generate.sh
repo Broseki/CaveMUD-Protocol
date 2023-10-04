34031af8319f213b3d9fa2926e53722ad9838b63
Broseki <broseki@cavefox.net>
2023-10-03 21:05:07 -0400

    Added login and header messages

--- generate.sh
+++ generate.sh
@@ -16,8 +16,11 @@ fi
 # Create the generated directory if it doesn't exist
 mkdir -p generated
 
+# Change the working directory to ./src
+cd src
+
 # Generate the source files for each .proto file in the src directory
-for file in src/*.proto
+for file in *.proto
 do
     filename=$(basename -- "$file")
     extension="${filename##*.}"
@@ -26,10 +29,10 @@ do
     # Generate the source files based on the provided argument
     case "$1" in
         cpp)
-            protoc --cpp_out=generated "$file"
+            protoc --cpp_out=../generated "$file"
             ;;
         python)
-            protoc --python_out=generated "$file"
+            protoc --python_out=../generated "$file"
             ;;
         *)
             echo "Invalid argument. Please provide a valid type of source files to generate (ex: cpp, python, etc)"
